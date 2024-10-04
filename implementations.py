1d125e30650d7cdb0d61d3b77218a05a28afda70
astaehli <100939730+astaehli@users.noreply.github.com>
2024-10-04 08:29:50 +0200

    Commit np.arr

--- implementations.py
+++ implementations.py
@@ -26,8 +26,6 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     return 2
 
 def calculate_loss_logistic(y, tx, w):
-    assert y.shape[0] == tx.shape[0]
-    assert tx.shape[1] == w.shape[0]
     pred = sigmoid(tx.dot(w))
     loss = y.T.dot(np.log(pred)) + (1 - y).T.dot(np.log(1 - pred))
     return np.squeeze(-loss).item() * (1 / y.shape[0])
@@ -57,11 +55,10 @@ def reg_logistic_regression(y, x, lambda_, initial_w, max_iters, gamma):
         # get loss and update w.
         loss, w_shaped = learning_by_gradient_descent_ridge(y_shaped, x, w_shaped, gamma, lambda_)
         # converge criterion
-        print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
         losses.append(loss)
         if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
             break
-    return w_shaped.ravel() , loss
+    return w_shaped.ravel(), np.array(loss)
 
 
 
